f149fcea0c557013e7b4df6664433f2ed326e5f6
Kirill Müller <krlmlr@mailbox.org>
2018-04-06 13:18:15 +0200

    support show argument

--- R/api-r-tee.R
+++ R/api-r-tee.R
@@ -1,19 +1,22 @@
 #' Run and tee R code
 #'
-#' Runs R code in an isolated session, writes output to the console and to a file.
+#' Runs R code in an isolated session, writes output to the console and to a
+#' file.  Forwards to [callr::r()].
 #'
 #' @inheritDotParams callr::r
 #' @inheritParams callr::r
 #' @param tee `[character]`\cr
 #'   Files to which output is written
-#' @param show `[NULL]`\cr
-#'   Do not set.
+#' @param show `[logical(1)]`\cr
+#'   Controls if standard output and standard error are shown on the console.
+#'   Defaults to `TRUE`, if you need `FALSE` consider using `callr::r()` with
+#'   the `stdout` and `stderr` arguments.
 #' @export
-r_tee <- function(..., tee = character(), callback = NULL, show = NULL) {
+r_tee <- function(..., tee = character(), callback = NULL, show = TRUE) {
   stopifnot(is.character(tee))
   if (!is.null(callback)) {
     stopifnot(is.function(callback), length(formals(callback)) == 1)
   }
-  stopifnot(is.null(show))
-  r_tee_impl(..., tee = tee, callback = callback)
+  stopifnot(is.logical(show), length(show) == 1)
+  r_tee_impl(..., tee = tee, callback = callback, show = show)
 }
--- R/r-tee.R
+++ R/r-tee.R
@@ -1,9 +1,11 @@
-r_tee_impl <- function(..., tee, callback) {
+r_tee_impl <- function(..., tee, callback, show) {
   files <- lapply(tee, file, "w")
   on.exit(lapply(files, close))
 
   my_callback <- function(line) {
-    cat(line, "\n", sep = "")
+    if (show) {
+      cat(line, "\n", sep = "")
+    }
     stripped_line <- fansi::strip_sgr(line)
     lapply(files, writeLines, text = stripped_line)
     if (!is.null(callback)) callback(line)
